baa4269d8a5e52f89fdc8e6192ddaf127345f3a3
Brandon Cheng <gluxon@users.noreply.github.com>
2018-06-07 23:01:52 -0400

    Remove unused .phone class

--- components/VContact.vue
+++ components/VContact.vue
@@ -1,6 +1,6 @@
 <template>
   <address>
-    <div className="phone">(860) 827-8988</div>
+    <div>(860) 827-8988</div>
     597 W Main St, New Britain, CT 06053
   </address>
 </template>
@@ -12,13 +12,5 @@ address {
   padding: 2em 0;
   color: rgb(110, 110, 110);
   line-height: 1.5;
-}
-
-.phone {
-  margin: 0.25em 0;
-
-  font-size: 1.5em;
-  font-family: monospace;
-  font-weight: bold;
 }
 </style>
