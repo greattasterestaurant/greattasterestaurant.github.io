685af47b10fb5f5d450c1c17c6715ff46ce729c1
Brandon Cheng <gluxon@users.noreply.github.com>
2018-06-11 21:49:27 -0400

    Add basic viewport for mobile devices

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,12 @@ module.exports = {
   plugins: ["@/plugins/vuex-router-sync.js"],
   modules: ["@nuxtjs/dotenv"],
   loading: false,
+  head: {
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" }
+    ]
+  },
   transition: {
     mode: "in-out"
   },
