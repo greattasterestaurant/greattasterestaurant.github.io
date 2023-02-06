44d98e841582ede437529634468c938fb2ef5e5f
Brandon Cheng <brandon.cheng@protonmail.com>
2023-02-06 03:35:53 -0500

    Disable import/named rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,10 @@ module.exports = {
   root: true,
   extends: [
     '@nuxtjs/eslint-config-typescript'
-  ]
+  ],
+  rules: {
+    // This seems to show false positive diagnostics. Disabling since TypeScript
+    // provides import safety out of the box.
+    'import/named': 'off'
+  }
 }
