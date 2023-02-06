f750fc5672b6b3814afaa4c82ebfe005fa3efc5b
Brandon Cheng <brandon.cheng@protonmail.com>
2023-02-06 03:18:56 -0500

    Update ESLint and use @nuxtjs/eslint-config-typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,6 @@
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
-  parserOptions: {
-    parser: "babel-eslint",
-  },
-  extends: ["eslint:recommended", "plugin:vue/recommended"],
-  plugins: ["vue"],
-  rules: {
-    semi: [2, "never"],
-    "vue/max-attributes-per-line": "off",
-  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript'
+  ]
 }
