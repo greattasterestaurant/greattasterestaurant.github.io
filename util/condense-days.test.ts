8048460fee3907832a06e47624c17bf9dc67ae2c
Brandon Cheng <gluxon@users.noreply.github.com>
2024-09-02 16:15:39 -0400

    Add test for condenseDays()

---
+++ util/condense-days.test.ts
@@ -0,0 +1,32 @@
+import { expect, test } from 'vitest'
+import { condenseDays } from './condense-days'
+
+test('condenses consecutive weekdays', () => {
+  const days = [
+    { dayOfWeek: 'Sunday', openTime: '12pm', closeTime: '9:30pm' },
+    { dayOfWeek: 'Monday', openTime: '11am', closeTime: '9:30pm' },
+    { dayOfWeek: 'Tuesday', openTime: '11am', closeTime: '9:30pm' },
+    { dayOfWeek: 'Wednesday', openTime: '11am', closeTime: '9:30pm' },
+    { dayOfWeek: 'Thursday', openTime: '11am', closeTime: '9:30pm' },
+    { dayOfWeek: 'Friday', openTime: '11am', closeTime: '10pm' },
+    { dayOfWeek: 'Saturday', openTime: '11am', closeTime: '10pm' }
+  ]
+
+  expect(condenseDays(days)).toEqual([
+    {
+      closeTime: '9:30pm',
+      dayOfWeek: 'Sunday',
+      openTime: '12pm'
+    },
+    {
+      closeTime: '9:30pm',
+      dayOfWeek: 'Monday–Thursday',
+      openTime: '11am'
+    },
+    {
+      closeTime: '10pm',
+      dayOfWeek: 'Friday–Saturday',
+      openTime: '11am'
+    }
+  ])
+})
